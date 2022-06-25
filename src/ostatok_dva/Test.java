f2d75d1d2389a785bfe83b2bcc724777b25618b8
Rustambek <aaa_ppp_1989@mail.ru>
2022-06-26 01:37:43 +0600

    first commit

---
+++ src/ostatok_dva/Test.java
@@ -0,0 +1,19 @@
+package ostatok_dva;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("a: ");
+        int a = in.nextInt();
+
+        System.out.println("b: ");
+        int b = in.nextInt();
+
+        System.out.println("Enter operation would you like: ");
+
+    }
+}
